c6e001d16ed2738563f6de95de0fd6576f93239c
Shibiby <muhammadfeisal10.mf@gmail.com>
2019-04-24 14:04:36 +0300

    commit

--- custom.js
+++ custom.js
@@ -1,3 +1,12 @@
 document.addEventListener('contextmenu' , function(event){
     event.preventDefault();
 })
+
+//Google Maps//
+function myMap() {
+    var mapProp = {
+        center:new google.maps.LatLng(-1.0476047,37.088323),
+        zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
